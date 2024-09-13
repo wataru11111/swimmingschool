190b1a044e76569024faaf51b645124924da09c0
Your Name <your-email@example.com>
2024-09-13 12:56:55 +0900

    二回目のコミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-
-  devise_for :customers,skip: [:passwords], controllers: {
+devise_for :customers, skip: [ :passwords ], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  devise_for :admin, skip: [ :registrations, :passwords ], controllers: {
   sessions: "admin/sessions"
 }
 
   namespace :admin do
     get '/' => "homes#top"
-    resources :calendar, only: [:new, :index, :edit, :create, :update]
-    resources :customers, only: [:show, :index, :edit,:update]
-
+    resources :calendar, only: [ :new, :index, :edit, :create, :update ]
+    resources :customers, only: [ :show, :index, :edit, :update ]
   end
 
   scope module: :public do
@@ -21,13 +19,13 @@ Rails.application.routes.draw do
     get 'dates/confirmation' => "date#confirmation"
     get 'dates/completion' => "date#completion"
 
-    resources :date, only: [:show, :index, :new, :create]
-    resources :offs, only: [:update, :create, :index, :new]
+    resources :date, only: [ :show, :index, :new, :create ]
+    resources :offs, only: [ :update, :create, :index, :new ]
 
 
     get '/calender/index' => "calendar#index"
 
-    resources :child, only: [:new,:edit,:update, :create]
+    resources :child, only: [ :new, :edit, :update, :create ]
 
     get '/customers/show' => "customers#show"
     get '/customers/information/edit' => "customers#edit"
@@ -36,7 +34,7 @@ Rails.application.routes.draw do
     patch 'customers/withdrawal' => "customers#withdrawal"
     get '/about' => "homes#about"
 
-    resources :items, only: [:index, :show]
+    resources :items, only: [ :index, :show ]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
