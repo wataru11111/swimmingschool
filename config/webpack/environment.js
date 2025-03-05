01c35ddf8d8e6c8d9e52f9f439494e565103b1f0
wataru11111 <setowataru1771@outlook.jp>
2025-03-05 18:38:40 +0900

    Linux/Ubuntuに変更してやっていく

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
+// Webpack 5 以降では `node` オプションを手動で設定しない
+// 必要であれば、'false' を指定して無効化
+environment.config.set('node', false)
+
 module.exports = environment
